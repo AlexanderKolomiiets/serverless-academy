1ce31734b9b3da518662148006ebe92623f212ff
Sasha Kolomiiets <kinbean444@gmail.com>
2023-02-14 22:47:11 +0200

    feat: implemented forecast features in currency bot

--- 05_telegram_exchange_bot/index.js
+++ 05_telegram_exchange_bot/index.js
@@ -3,8 +3,21 @@ import TelegramBot from 'node-telegram-bot-api';
 
 const chatId = 403311177;
 const token = '6011878378:AAFVk5EcQt7iFeCxQJxOg9liOUNqaPFLIU0';
-const privatBankApi = 'https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=11';
 const bot = new TelegramBot(token, { polling: true });
+
+const privatBankApi = 'https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=11';
+const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast';
+const apiKey = '38d057bd87e726b8b5809fc8240e076a';
+const city = 'Kyiv';
+
+const weatherUrl = `${WEATHER_BASE_URL}?q=${city}&appid=${apiKey}`;
+
+const kelvinToCelsius = (temp) => (temp - 273.15).toFixed(2);
+
+const getWeatherForecast = async () => {
+  const res = await axios.get(weatherUrl);
+  return res.data.list.slice(0, 12);
+};
 
 const getExchangeRates = async (currency) => {
   const res = await axios.get(privatBankApi);
@@ -19,28 +32,83 @@ const getExchangeRates = async (currency) => {
   `;
 };
 
-bot.sendMessage(chatId, 'Privat exchange rates:', {
+bot.sendMessage(chatId, 'Welcome, choose an option', {
   reply_markup: {
-    keyboard: [['Currency rates']],
+    keyboard: [[`Forecast in ${city}`], ['Currencies']],
     resize_keyboard: true,
   },
 });
 
-bot.onText(/Currency rates/, () => {
-  bot.sendMessage(chatId, 'Select currency:', {
+bot.on('message', (msg) => {
+  if (msg.text === `Forecast in ${city}`) {
+    bot.sendMessage(msg.chat.id, 'Select time interval:', {
+      reply_markup: {
+        keyboard: [['at intervals of 3 hours'], ['at intervals of 6 hours'], ['Previous menu']],
+        resize_keyboard: true,
+      },
+    });
+  }
+});
+
+bot.on('message', async (msg) => {
+  let interval;
+  if (msg.text === 'at intervals of 3 hours') {
+    interval = 3;
+  }
+  if (msg.text === 'at intervals of 6 hours') {
+    interval = 6;
+  }
+  if (interval === 3 || interval === 6) {
+    const forecast = await getWeatherForecast();
+    let data = '';
+    for (let i = 0; i < forecast.length; i += interval / 3) {
+      const {
+        main: {
+          temp,
+          temp_max,
+          temp_min,
+          humidity,
+          pressure,
+        },
+        wind: { speed },
+        dt_txt,
+      } = forecast[i];
+      data += `📆 Current date: ${dt_txt}\n`
+      + `🌡️ Temperature: ${kelvinToCelsius(temp)}°C\n`
+      + `📈 Max temp: ${kelvinToCelsius(temp_max)}°C\n`
+      + `📉 Min temp: ${kelvinToCelsius(temp_min)}°C\n`
+      + `💧 Umidity: ${humidity}%\n`
+      + `📊 Pressure: ${pressure} hPa\n`
+      + `💨 Wind: ${speed} Km/h\n\n`;
+    }
+
+    bot.sendMessage(msg.chat.id, data);
+  }
+});
+
+bot.on('message', (msg) => {
+  if (msg.text === 'Currencies') {
+    bot.sendMessage(msg.chat.id, 'Select currency:', {
+      reply_markup: {
+        keyboard: [['USD'], ['EUR'], ['Previous menu']],
+        resize_keyboard: true,
+      },
+    });
+  }
+});
+
+bot.on('message', async (msg) => {
+  if (msg.text === 'USD' || msg.text === 'EUR') {
+    const rates = await getExchangeRates(msg.text);
+    bot.sendMessage(msg.chat.id, rates);
+  }
+});
+
+bot.onText(/Previous menu/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Please, choose an option', {
     reply_markup: {
-      keyboard: [['USD'], ['EUR']],
+      keyboard: [[`Forecast in ${city}`], ['Currencies']],
       resize_keyboard: true,
     },
   });
-});
-
-bot.onText(/USD/, async (data) => {
-  const rates = await getExchangeRates(data.text);
-  bot.sendMessage(chatId, rates);
-});
-
-bot.onText(/EUR/, async (data) => {
-  const rates = await getExchangeRates(data.text);
-  bot.sendMessage(chatId, rates);
 });
